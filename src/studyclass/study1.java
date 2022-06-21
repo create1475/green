d4b4c2eb5264ad4560054a9b5d52058e45731463
seojun oh <osj1475@naver.com>
2022-06-21 23:20:28 +0900

    study2

---
+++ src/studyclass/study1.java
@@ -0,0 +1,14 @@
+package studyclass;
+
+public class study1 {
+
+	public static void main(String[] args) {
+
+		int x = 1;
+		int y = 2;
+		int z = 5;
+		System.out.println(x+y+z);
+		
+	}
+
+}
---
+++ src/studyclass/study2.java
@@ -0,0 +1,13 @@
+package studyclass;
+
+public class study2 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int z = 5;
+		System.out.println(x+y+z);
+		
+	}
+
+}
