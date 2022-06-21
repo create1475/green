7092dcc982b3da3cdb6bdfb26625dccde389df8b
seojun oh <osj1475@naver.com>
2022-06-21 23:11:41 +0900

    # WARNING: head commit changed in the meantime
    Change-Id: Ide8784b2af0347b74008d61af877c2b2e3b73167

---
+++ src/study.java
@@ -0,0 +1,14 @@
+
+public class study {
+
+	public static void main(String[] args) {
+
+		
+		int x = 1;
+		int y = 2;
+		
+		System.out.println(x+y);
+		
+	}
+
+}
---
+++ src/studyclass/study11.java
@@ -0,0 +1,13 @@
+package studyclass;
+
+public class study11 {
+
+	public static void main(String[] args) {
+
+		int x = 1;
+		int y = 1;
+		System.out.println(x+y);
+		
+	}
+
+}
