8dba489e5212f576fa856f6ed5c0c97de851eb1d
seojun oh <osj1475@naver.com>
2022-06-24 12:52:38 +0900

    2022/06/24

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,10 +1,22 @@
 package practice;
 
 
+
 public class Main {
 
 	public static void main(String[] args) {
+		double Pi = 3.141592;
 
+//		System.out.println(Pi);
+//		System.out.println(Pi*1000);
+//		System.out.println((Pi*1000)/1000);
+		System.out.println(Math.round((Pi*1000))/1000.0);
+		System.out.println(Math.round((Pi*1000))/1000);
+		System.out.println(Math.round(Pi*1000));
+		
+		//		double shortPi = Math.round(Pi*1000)/1000.0;
+//		System.out.println(shortPi);
+		
 		
 		
 		}
